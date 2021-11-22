fe418778e96ad992b21468134544773f8d1248bb
pkuipers1 <54790202+pkuipers1@users.noreply.github.com>
2021-11-22 15:13:59 +0100

    Add files via upload

---
+++ Database/Database/Database.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Database
+{
+    class Database
+    {
+        private string name;
+        public Database(string aName)
+        {
+            name = aName;
+        }
+    }
+}
